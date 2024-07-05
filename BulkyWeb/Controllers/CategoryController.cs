61efbefa53f7e62736fbaf2510d08f6514235a5c
Vladislava Diachenko <v.diachenko@ukma.edu.ua>
2024-07-05 21:41:03 +0100

    Repository interface + category repository added

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,19 +1,21 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using BulkyWeb.Repository.IRepository;
+using BulkyWeb.Repository;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository db)
         {
-            _db = db;
+            _categoryRepo = db;
         }
         public IActionResult Index()
         {
-            var objCategoryList = _db.Categories.ToList();
+            var objCategoryList = _categoryRepo.GetAll().ToList();
             return View(objCategoryList);
 
         }
@@ -33,8 +35,8 @@ namespace BulkyWeb.Controllers
 
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj); 
-                _db.SaveChanges();
+                _categoryRepo.Add(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Category");
             }
@@ -48,7 +50,7 @@ namespace BulkyWeb.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);          /*(some more options how to do it)
+            Category? categoryFromDb = _categoryRepo.Get(u=>u.Id==id);          /*(some more options how to do it)
                                                                           Category categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
                                                                           Category categoryFromDb2 = _db.Categories.Where(u => u.Id ==id).FirstOrDefault();*/
             if (categoryFromDb == null)
@@ -63,8 +65,8 @@ namespace BulkyWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
+                _categoryRepo.Update(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index", "Category");
             }
@@ -77,7 +79,7 @@ namespace BulkyWeb.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);          
+            Category? categoryFromDb = _categoryRepo.Get(u=>u.Id == id);          
             if (categoryFromDb == null)
             {
                 return NotFound();
@@ -88,13 +90,13 @@ namespace BulkyWeb.Controllers
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
-            Category? obj = _db.Categories.Find(id);
+            Category? obj = _categoryRepo.Get(u => u.Id == id);
             if (obj == null)
             {
                 return NotFound();
             }
-            _db.Categories.Remove(obj);
-            _db.SaveChanges();
+            _categoryRepo.Remove(obj);
+            _categoryRepo.Save();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index", "Category");
            
---
+++ BulkyWeb/Repository/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using BulkyWeb.Models;
+using BulkyWeb.Repository.IRepository;
+
+namespace BulkyWeb.Repository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
