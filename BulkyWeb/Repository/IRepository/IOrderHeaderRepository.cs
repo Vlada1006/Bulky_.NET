2ea6bbc798cf10faa327845347cbe4d0e0902889
Vladislava Diachenko <v.diachenko@ukma.edu.ua>
2024-11-16 21:32:28 +0000

    Order confirmation section is completed

--- BulkyWeb/Repository/IRepository/IOrderHeaderRepository.cs
+++ BulkyWeb/Repository/IRepository/IOrderHeaderRepository.cs
@@ -7,5 +7,6 @@ namespace BulkyWeb.Repository.IRepository
     {
         void Update(OrderHeader obj);
         void UpdateStatus(int id, string orderStatus, string paymentStatus = null);
+        void UpdateStripePaymentID(int id1, string id2, string paymentIntentId);
     }
 }
