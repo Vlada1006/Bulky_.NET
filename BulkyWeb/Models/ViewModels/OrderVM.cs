b85a4ca81d0a8e0e9dbd7ee1523de393853bfcd9
Vladislava Diachenko <v.diachenko@ukma.edu.ua>
2024-11-19 22:34:03 +0000

    Order Management page is in process

---
+++ BulkyWeb/Models/ViewModels/OrderVM.cs
@@ -0,0 +1,10 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyWeb.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
